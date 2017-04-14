95ec734d9196e311bf73c0f13b418aa0dc199f7b
yongqiang <netqyq@gmail.com>
2017-04-14 14:52:30 +0800

    refactor: Make Coordinator like a daemon process using waitGroup.

--- src/distributed/coordinator/exec/main.go
+++ src/distributed/coordinator/exec/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"distributed/coordinator"
 	//"fmt"
+	"sync"
 )
 
 var dc *coordinator.DatabaseConsumer
 var wc *coordinator.WebappConsumer
 
 func main() {
+	// wait, like a daemon
+	var wg sync.WaitGroup
+	wg.Add(1)
+
 	ea := coordinator.NewEventAggregator()
 	dc = coordinator.NewDatabaseConsumer(ea)
 	wc = coordinator.NewWebappConsumer(ea)
@@ -17,5 +22,6 @@ func main() {
 
 	// var a string
 	// fmt.Scanln(&a)
-	for {}
+
+	wg.Wait()
 }
